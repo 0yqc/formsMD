7c41a6589fde1f0e40262d5ad7cfc02ce644f857
0yqc <0yqc@duck.com>
2025-06-29 17:50:24 +0200

    continued on the conversion: input opening and filtering out complete fMD blocks is done

--- core_convert/main.py
+++ core_convert/main.py
@@ -1,21 +1,18 @@
-import compile # local file for compiling specific blocks
-import sys # get command line arguments
+import compile # local file for compiling the document
+import argparse # get command line arguments
 import os # join file path
 
 # --- OPEN INPUT --- #
 
+inp_file = os.path.join(os.getcwd(), "input.fmd") # set defualt input file, unless overwritten
+out_file = os.path.join(os.getcwd(), "output/") # set default output directory, unsless overwritten
 
-try: # if there is an argument for the working directory
-	inp_dir = os.path.join(sys.argv[1], "input/")
-except: # assume current working directory
-	inp_dir = "input/"
+parser = argparse.ArgumentParser(description="core conversion of formsMD files",epilog="For more help you can reach out to 0yqc@duck.com") # set-up for autmatic help, etc.
+parser.add_argument("-i", "--inp", "--input", dest="input", help="path of the input file. Defaults to working_dir/input.fmd") # add argument --input to define the input file location
+parser.add_argument("-o", "--out", "--output", dest="output", help="directory where the output HTML files should be saved. Defualts to working_dir/output/") # add argument --output to define the output directory
+args = parser.parse_args() # get arguments
+inp_file = args.input # overwritte the default if it has been set in the args
+out_dir = args.output # overwritte the default if it has been set in the args 
 
-inp_ids = [] # init for filling in the IDs
-for i in range(len(os.listdir(inp_dir))): # loop through every file in the input directory
-	current = os.listdir(inp_dir)[i] # set the current file name as a var for further use
-	if current.endswith(".fmd") and current.startswith("page_"): # only use files that start with page_ and end with .fmd (e.g. page_1.fmd)
-		inp_ids.append(current.removeprefix("page_").removesuffix(".fmd")) # remove the parts mentioned above to only have the ID left
-
-inp = []
-for i in range(len(inp_ids)):
-	inp[i] = open(inp_dir + "/page_" + inp_ids + ".fmd")
+html = compile.file(inp_file) # start the compiling (./compile.py)
+print(html) # print the resulting HTML

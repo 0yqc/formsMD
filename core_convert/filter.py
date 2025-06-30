cce750d4411012c155f253d9a129eb43d6496420
0yqc <0yqc@duck.com>
2025-06-30 08:39:20 +0200

    finished compiling for simple checkbox questions (w/o options yet)

--- core_convert/filter.py
+++ core_convert/filter.py
@@ -1,3 +1,5 @@
+import compiler # local file to compile single blocks
+
 def file(file):
 	'''
 	file is the path to the file
@@ -5,28 +7,31 @@ def file(file):
 	this function compiles .fmd files into HTML files and returns a string for further use.
 	'''
 	# clone all lines into a list
-	with open(file, "r") as file:
+	with open(file, 'r') as file:
 		lines = file.readlines()
 	for n, i in enumerate(lines): # n is a counter (0,1,2,...), i is the current line
-		if i.startswith("?"): # a block starts
+		if i.startswith('?'): # a block starts
 			block_construction = True # indicator that the block is being constructed
 			block_str = i # add the first line of the block to the block string
-			lines[n] = "" # clear the current line
-		elif i.startswith("\t") and block_construction == True: # a block continues (only applicable if it's in construction)
+			lines[n] = '' # clear the current line
+		elif i.startswith('\t') and block_construction == True: # a block continues (only applicable if it's in construction)
 			block_str = block_str + i # append new lines to the block
-			lines[n] = "" # clear the current line
+			lines[n] = '' # clear the current line
 		else: # if it's normal markdown no further processing should be done
 			continue
 		try: # lines[n+1] would fail if it's the last line
-			if not lines[n+1].startswith("\t"): # if the block does not continue
+			if not lines[n+1].startswith('\t'): # if the block does not continue
 				block_construction = False # stop the construction
 				lines[n] = block(block_str) # compile the block and write it all into the last line
 		except: # it's the last line, so it must be compiled one last time
 			block_construction = False # stop the construction
 			lines[n] = block(block_str) # compile the block
-	return("".join(lines)) # return everything at the end, joined together
+	return(''.join(lines)) # return everything at the end, joined together
 
-
+html = None # init
 def block(text: str): # block compiling logic
-	text = text.replace("?","!") # test to see if it executes
+	if text.count('\n\t[]') or text.count('\n\t[ ]') or text.lower().count('\n\t[x]'): # checkbox question (text.count() == 0 is false)
+		text = compiler.checkbox(text)
+	elif text.count('\n\t()') or text.count('\n\t( )') or text.lower().count('\n\t(x)'): # multiple-choice question
+		compiler.multiple(text)
 	return text
--- core_convert/main.py
+++ core_convert/main.py
@@ -1,18 +1,18 @@
-import compile # local file for compiling the document
+import filter # local file for compiling the document
 import argparse # get command line arguments
 import os # join file path
 
 # --- OPEN INPUT --- #
 
-inp_file = os.path.join(os.getcwd(), "input.fmd") # set defualt input file, unless overwritten
-out_file = os.path.join(os.getcwd(), "output/") # set default output directory, unsless overwritten
+inp_file = os.path.join(os.getcwd(), 'input.fmd') # set defualt input file, unless overwritten
+out_file = os.path.join(os.getcwd(), 'output/') # set default output directory, unsless overwritten
 
-parser = argparse.ArgumentParser(description="core conversion of formsMD files",epilog="For more help you can reach out to 0yqc@duck.com") # set-up for autmatic help, etc.
-parser.add_argument("-i", "--inp", "--input", dest="input", help="path of the input file. Defaults to working_dir/input.fmd") # add argument --input to define the input file location
-parser.add_argument("-o", "--out", "--output", dest="output", help="directory where the output HTML files should be saved. Defualts to working_dir/output/") # add argument --output to define the output directory
+parser = argparse.ArgumentParser(description='core conversion of formsMD files',epilog='For more help you can reach out to 0yqc@duck.com') # set-up for autmatic help, etc.
+parser.add_argument('-i', '--inp', '--input', dest='input', help='path of the input file. Defaults to working_dir/input.fmd') # add argument --input to define the input file location
+parser.add_argument('-o', '--out', '--output', dest='output', help='directory where the output HTML files should be saved. Defualts to working_dir/output/') # add argument --output to define the output directory
 args = parser.parse_args() # get arguments
 inp_file = args.input # overwritte the default if it has been set in the args
 out_dir = args.output # overwritte the default if it has been set in the args 
 
-html = compile.file(inp_file) # start the compiling (./compile.py)
+html = filter.file(inp_file) # start the compiling (./filter.py)
 print(html) # print the resulting HTML
